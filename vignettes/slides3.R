31c05b88bbe9a07970cfd7d08aaff1e6840ebd79
jamieRowen <jamie@jumpingrivers.com>
2018-05-25 13:28:42 +0100

    removed slides source

--- vignettes/slides3.R
+++
@@ -1,6 +0,0 @@
-## ----eval=FALSE----------------------------------------------------------
-#  vignette("chapter3", package="jrShiny")
-
-## ----eval=FALSE----------------------------------------------------------
-#  vignette("chapter3", package="jrShiny")
-
