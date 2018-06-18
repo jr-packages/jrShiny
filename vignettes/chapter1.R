b7d9c8884fe8faf97952233f7b9bcb96d4393f8a
Jamie <jamie@jumpingrivers.com>
2018-06-18 12:27:01 +0100

    moved and renamed /inst and /vignettes to be appropriate now that course split rfom reporting

--- vignettes/chapter1.R
+++
@@ -1,7 +0,0 @@
-## ----echo=FALSE, message=FALSE-------------------------------------------
-library("jrShiny")
-
-## ----eval=FALSE----------------------------------------------------------
-#  ## Hope this should work!
-#  install.packages("revealjs")
-
--- vignettes/chapter2.R
+++
@@ -1,7 +0,0 @@
-## ----echo=FALSE, message=FALSE-------------------------------------------
-library("jrShiny")
-library("flexdashboard")
-
-## ----eval=FALSE----------------------------------------------------------
-#  vignette("htmlwidgets", package="jrShiny")
-
