066eaaba4c24983d79deaaa77fafc5865c3bae92
John McIntyre <john@jumpingrivers.com>
2019-12-12 16:23:31 +0000

    lintr changes

--- R/layout_show.R
+++ R/layout_show.R
@@ -9,8 +9,8 @@
 layout_show = function(n = 1, cex=1, text = NULL) {
   if (is.null(text)) text = paste0("Chart ", seq_len(n))
   oma_saved <- par("oma")
-  par(oma = rep.int(0, 4))
-  par(oma = oma.saved)
+  par(oma = rep_int(0, 4))
+  par(oma = oma_saved)
   o_par <- par(mar = rep_int(0, 4))
   on_exit(par(o_par))
   for (i in seq_len(n)) {
